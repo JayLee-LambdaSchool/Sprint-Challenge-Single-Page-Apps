960f3460b0cd17404a7465ece7e2aeaf60d7fa50
dreampoetlee <dreampoetlee@gmail.com>
2019-12-15 18:04:09 -0500

    Worked on Sprint Challenge - Updated the following components: CharacterCard & CharacterList - Need to update the following components: SearchForm & WelcomePage

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Cards = styled.div`
+  border: 2px solid black;
+  margin: 3%;
+  background-color: #83D2E4;
+  border-radius: 10px;
+  padding: 3%;
+`;
+
+export default function CharacterCard(name, species, gender) {
+  return (
+    <Cards>
+      <h3>Name:{name}</h3>
+      <p>Species:{species}</p>
+      <p>Gender:{gender}</p>
+    </Cards>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,48 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
+import styles from "styled-components";
+import {Link} from "react-router-dom";
+
+const Buttons = styles.button`
+  margin: 10px;
+  background-color: #3D373C;
+  color: #E7E0DB;
+`;
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [newCharacter, setNewCharacter] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    useEffect(() => {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")    
+        .then(response => {
+          setNewCharacter(response.data.results);
+          console.log(response);
+        })
+        .catch(error => {
+          console.error("Server Error", error);
+        });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <Link to="/">
+        <Buttons>Home</Buttons>
+      </Link>
+
+      <Link to="/Search">
+        <Buttons>Search</Buttons>
+      </Link>
+
+      {newCharacter.map(ind => (
+        <CharacterCard
+          key={ind.id}
+          name={ind.name}
+          species={ind.species}
+          gender={ind.gender}
+        />
+      ))}
     </section>
   );
 }
